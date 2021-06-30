0404bf1c627245cb35117ae2a153f08d1f2a85aa
Tyler Mandry <tmandry@gmail.com>
2021-06-29 23:30:20 -0500

    Draw image on screen and save to file

--- crab_ray/src/main.rs
+++ crab_ray/src/main.rs
@@ -1,3 +1,29 @@
+use std::{slice, thread, time::Duration};
+use image::{Bgra as Pixel, DynamicImage::ImageBgra8 as DynImage, ImageBuffer};
+use minifb::{Window, WindowOptions};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = ImageBuffer::<Pixel<u8>, Vec<u8>>::new(640, 480);
+    for (i, pixel) in image.pixels_mut().enumerate() {
+        let color = i as u32;
+        *pixel = Pixel(color.to_le_bytes());
+    }
+
+    let mut window = Window::new(
+        "crab rayve",
+        image.width() as usize,
+        image.height() as usize,
+        WindowOptions::default())
+        .unwrap();
+
+    let buf: &[u8] = &*image;
+    let buf: &[u32] = unsafe { slice::from_raw_parts(buf.as_ptr() as _, buf.len() / 4) };
+
+    window.update();
+    while window.is_open() {
+        window.update_with_buffer(buf, image.width() as _, image.height() as _).unwrap();
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    DynImage(image).into_rgb8().save("test.png").unwrap();
 }
