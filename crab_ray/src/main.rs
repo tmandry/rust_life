549871fa76287ec0c0d5af8820bbf61a1d5e7250
Tyler Mandry <tmandry@gmail.com>
2021-07-03 17:58:54 -0500

    Extract render function

--- crab_ray/src/main.rs
+++ crab_ray/src/main.rs
@@ -2,12 +2,17 @@ use std::{slice, thread, time::Duration};
 use image::{Bgra as Pixel, DynamicImage::ImageBgra8 as DynImage, ImageBuffer};
 use minifb::{Window, WindowOptions};
 
-fn main() {
-    let mut image = ImageBuffer::<Pixel<u8>, Vec<u8>>::new(640, 480);
+type Image = ImageBuffer::<Pixel<u8>, Vec<u8>>;
+
+fn render(image: &mut Image) {
     for (i, pixel) in image.pixels_mut().enumerate() {
         let color = i as u32;
         *pixel = Pixel(color.to_le_bytes());
     }
+}
+
+fn main() {
+    let mut image = Image::new(640, 480);
 
     let mut window = Window::new(
         "crab rayve",
@@ -15,6 +20,8 @@ fn main() {
         image.height() as usize,
         WindowOptions::default())
         .unwrap();
+
+    render(&mut image);
 
     let buf: &[u8] = &*image;
     let buf: &[u32] = unsafe { slice::from_raw_parts(buf.as_ptr() as _, buf.len() / 4) };
